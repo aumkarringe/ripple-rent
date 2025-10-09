fa7dac3381fba32ca20b7b754b9ecf68da054282
aumkarringe <aumkarringe@gmail.com>
2025-10-09 01:11:30 -0700

    updates done

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ripple-rent/", // 👈 IMPORTANT for GitHub Pages
   server: {
     host: "::",
     port: 8080,
